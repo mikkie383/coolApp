796ea1323b2a08dc7a7cafe800a7f6fe431da850
mikkie383 <tsa16002@byui.edu>
2020-09-18 19:17:12 -0600

    add js

---
+++ select.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'What is Idaho area code?',
+        a: '110',
+        b: '911',
+        c: '208',
+        d: '385',
+        correct: 'c'
+    },{
+        question: 'Where does BYU-Idaho locate?',
+        a: 'Rexburg',
+        b: 'Idaho Falls',
+        c: 'Twin Falls',
+        d: 'Boise',
+        correct: 'a'
+    },{
+        question: 'HTML stands for?',
+        a: 'Hey Taylor Misses Larry',
+        b: 'Hypertext Markup Language',
+        c: 'Hippie Travel Mobile Land',
+        d: 'Homo Toby Married Lesbian',
+        correct: 'b'
+    },{
+        question: 'How would you say Hello in Mandarin?',
+        a: 'Wowow',
+        b: 'LeHo',
+        c: 'Ni2Hao3',
+        d: 'LiHo',
+        correct: 'c'
+    },{
+        question: 'What is the name of worldwide virus in 2020',
+        a: 'Mimo-20',
+        b: 'Covid-19',
+        c: 'Trump-19',
+        d: 'Wuhan-20',
+        correct: 'b'
+    }
+]
