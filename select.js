8737a784de2cc995b0345dc46732f48134aa0d9b
mikkie383 <tsa16002@byui.edu>
2020-09-20 08:09:06 -0600

    Quiz Complete!

--- select.js
+++ select.js
@@ -35,4 +35,62 @@ const quizData = [
         d: 'Wuhan-20',
         correct: 'b'
     }
-]
+];
+const quiz = document.getElementById('quiz');
+const questionEl = document.getElementById('question');
+const answerEl = document.querySelectorAll('.option');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+let currentQuestion = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz(){
+    deselectAnswer();
+    const currentQuizData = quizData[currentQuestion];
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+
+}
+function getSelected(){
+    
+    let answer = undefined;
+    answerEl.forEach((answerEl) => {
+        if(answerEl.checked){
+            answer = answerEl.id;
+        }
+    });
+    return answer;
+}
+function deselectAnswer(){
+    answerEl.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+submitBtn.addEventListener('click', () => {
+    //check to see the answer
+    const answer = getSelected();
+
+    if(answer){
+        if(answer === quizData[currentQuestion].correct){
+            score++;
+        }
+
+        currentQuestion++;
+        if(currentQuestion < quizData.length){
+            loadQuiz();
+        } else{
+            //TODO: Show results
+            quiz.innerHTML = `<h2>You got ${score} out of ${quizData.length}.</h2><button onClick="location.reload()">Reload</button>`
+        }
+    }
+    
+});
